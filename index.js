d4a97f4c1a4960701651d3b06a53823c3e43185e
maks <masksnytko@users.noreply.github.com>
2017-10-19 00:22:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,68 @@
-class Bind {
-    constructor(data, template) {
+function bind(node, data, _data) {
+    for (var i = 0; i < node.attributes.length; i++) {
+        let atr = node.attributes[i];
+        let {name, value} = atr;
         
-        if (typeof data !== 'object') {
-            throw new Error('data param not support');
-        }
-
-        if (typeof template === 'string') {
-            let el = document.createElement('div');
-            el.innerHTML = template;
-            this.el = el.children[0];
-        } else if (template instanceof Element) {
-            this.el = template;
-        } else {
-            throw new Error('template param not support');
-        }
-
-        let bind = (el, data, _data) => {
-            for (var i = 0, j; i < el.children.length; i++) {
-                let node = el.children[i];
-
-                for (j = 0; j < node.attributes.length; j++) {
-                    let atr = node.attributes[j];
-                    let {name, value} = atr;
-                    
-                    if (data[value]) {
-                        if (typeof data[value] === 'function') {
-                            node[name] = data[value].bind(data);
-                        } else if (value !== '') {
-                            
-                            if (data[value] !== undefined) {
-                                _data[name] = data[value];
-                                node.setAttribute(name, data[value]);    
-                            }
-
-                            Object.defineProperty(data, value, {
-                                get: () => _data[value],
-                                set: v => {
-                                    _data[value] = v;
-                                    node.setAttribute(name, data[value]);
-                                }
-                            });
-                        }
-                    } else if (data[name] !== undefined) {
-                        _data[name] = data[name];
-                        node.textContent = data[name];
-                        
-                        this[name] = node;
-                        
-                        Object.defineProperty(data, name, {
-                            get: () => _data[name],
-                            set: v => {
-                                _data[name] = v
-                                node.textContent = v;
-                            }
-                        });
-                    }
+        if (data.hasOwnProperty(value)) {
+            if (typeof data[value] === 'function') {
+                node[name] = data[value].bind(data);
+            } else if (value !== '') {
+                
+                if (data[value] !== undefined) {
+                    _data[name] = data[value];
+                    node.setAttribute(name, data[value]);    
                 }
-		        bind(node, data, _data);
-            }
-        };
 
-        bind(this.el, data, new Object);
+                Object.defineProperty(data, value, {
+                    get: () => _data[value],
+                    set: v => {
+                        _data[value] = v;
+                        node.setAttribute(name, data[value]);
+                    }
+                });
+            }
+        } else {
+            if (data.hasOwnProperty(name)) {
+                _data[name] = data[name];
+                node.textContent = data[name];
+                Object.defineProperty(data, name, {
+                    get: () => _data[name],
+                    set: v => {
+                        _data[name] = v
+                        node.textContent = v;
+                    }
+                });
+            }
+            if (value === '') {
+                data.el[name] = node;
+            }
+        }
     }
+
+    for (i = 0; i < node.children.length; i++) {
+        bind(node.children[i], data, _data);
+    }
+};
+
+function Bind (data, template) {
+
+    if (typeof data !== 'object' && typeof data !== 'function') {
+        throw new Error('data param not support');
+    }
+
+    let el;
+    if (typeof template === 'string') {
+        el = document.createElement('div');
+        el.innerHTML = template;
+        el = el.children[0];
+    } else if (template instanceof Element) {
+        el = template;
+    } else {
+        throw new Error('template param not support');
+    }
+
+    data.el = new Object;
+    bind(el, data, new Object);
 }
 
 module.exports = Bind;
